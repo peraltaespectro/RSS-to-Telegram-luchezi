34095f0170eb52a342626c64127844e5afd64db7
Peralta Espectro <109923346+peraltaespectro@users.noreply.github.com>
2022-09-05 15:38:26 -0300

    Update post_formatter.py

--- src/parsing/post_formatter.py
+++ src/parsing/post_formatter.py
@@ -404,7 +404,7 @@ class PostFormatter:
             elif via_type == BARE_LINK_VIA and self.link:
                 via_text = Text(self.link)
             elif via_type == TEXT_LINK_VIA and self.link:
-                via_text = Link(title, param=self.link)
+                via_text = Link('🔗 Link', param=self.link)
             else:
                 via_text = None
             via_html = via_text.get_html() if via_text else None
@@ -449,7 +449,7 @@ class PostFormatter:
             elif via_type == BARE_LINK_VIA and self.link:
                 sourcing_html = self.link
             else:  # NORMAL_MESSAGE
-                sourcing_html = Link(title, param=self.link).get_html() if self.link else None
+                sourcing_html = Link('🔗 Link', param=self.link).get_html() if self.link else None
 
             header = (
                     (feed_title_html or '')
